03eafde0182322b95bb478ea394756c4a18d277a
Ashokdy <ashokdy@gmail.com>
2025-07-01 11:18:39 -0400

    Initial Commit

---
+++ SwiftUIBugs/SwiftUIBugsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBugsApp.swift
+//  SwiftUIBugs
+//
+//  Created by ASHOK DY on 2025-07-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBugsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
